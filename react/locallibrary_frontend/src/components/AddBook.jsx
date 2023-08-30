a1edecdffdc1d6309d21dc7cabe4d97cdb126785
fkbad <fkb@uchicago.edu>
2023-08-30 10:58:31 -0500

    added basic functioning post request on BookForm submission

--- react/locallibrary_frontend/src/components/AddBook.jsx
+++ react/locallibrary_frontend/src/components/AddBook.jsx
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import BookForm from './BookForm';
 
 const AddBook = () => {
   const handleOnSubmit = (book) => {
     console.group("in AddBook submit handler)")
     console.info("received", book, "from child Book Form")
+
+    let POST_URL = "http://localhost:8000/api/books/"
+    axios.post(`${POST_URL}`, book)
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
+    console.groupEnd()
+
   };
 
   return (
